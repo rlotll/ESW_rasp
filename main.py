cf6e7441a6a4be84f6bc7ef07eaf881919c5dfe3
KGB-rasp <rlarmdql@gmail.com>
2024-12-05 17:34:51 +0900

    fire&water

--- main.py
+++ main.py
@@ -57,78 +57,71 @@ class Joystick:
         self.width = self.disp.width
         self.height = self.disp.height
 
+#시작 세팅
 joystick = Joystick()
-
 my_image = Image.new("RGB", (joystick.width, joystick.height))
-
 my_draw = ImageDraw.Draw(my_image)
 
+#검은 직사각형 그리기(화면 초기화)
 my_draw.rectangle((0, 0, joystick.width, joystick.height), fill=(10, 100, 30, 100))
 
+#### 세팅
+
+
+# Draw a black filled box to clear the image.
+my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
 joystick.disp.image(my_image)
 
-my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
-my_draw.ellipse((100, 50, 140, 90), outline = "#FFFFFF", fill = (0, 0, 0))
-#좌표는 동그라미의 왼쪽 위, 오른쪽 아래 점 (x1, y1, x2, y2)
-joystick.disp.image(my_image)
+#색 지정 팔레트
+udlr_fill = "#00FF00"
+udlr_outline = "#00FFFF"
+button_fill = "#FF00FF"
+button_outline = "#FFFFFF"
 
-import numpy as np
-class Character:
-    def __init__(self, width, height):
-        self.appearance = 'circle'
-        self.state = None
-        self.position = np.array([width/2 - 20, height/2 - 20, width/2 + 20, height/2 + 20])
-        self.outline = "#FFFFFF"
+fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
 
-    def move(self, command = None):
-        if command == None:
-            self.state = None
-            self.outline = "#FFFFFF" #검정색상 코드!
-        
-        else:
-            self.state = 'move'
-            self.outline = "#FF0000" #빨강색상 코드!
-
-            if command == 'up_pressed':
-                self.position[1] -= 5
-                self.position[3] -= 5
-
-            elif command == 'down_pressed':
-                self.position[1] += 5
-                self.position[3] += 5
-
-            elif command == 'left_pressed':
-                self.position[0] -= 5
-                self.position[2] -= 5
-                
-            elif command == 'right_pressed':
-                self.position[0] += 5
-                self.position[2] += 5
-
-# 잔상이 남지 않는 코드
-my_circle = Character(joystick.width, joystick.height)
-my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
 while True:
-    command = None
+    command = {'move': False, 'up_pressed': False , 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
+    
     if not joystick.button_U.value:  # up pressed
-        command = 'up_pressed'
+        command['up_pressed'] = True
+        command['move'] = True
 
-    elif not joystick.button_D.value:  # down pressed
-        command = 'down_pressed'
+    if not joystick.button_D.value:  # down pressed
+        command['down_pressed'] = True
+        command['move'] = True
 
-    elif not joystick.button_L.value:  # left pressed
-        command = 'left_pressed'
+    if not joystick.button_L.value:  # left pressed
+        command['left_pressed'] = True
+        command['move'] = True
 
-    elif not joystick.button_R.value:  # right pressed
-        command = 'right_pressed'
-        
-    else:
-        command = None
+    if not joystick.button_R.value:  # right pressed
+        command['right_pressed'] = True
+        command['move'] = True
+
+    if not joystick.button_A.value: # A pressed
+        bullet = Bullet(my_circle.center, command)
+        bullets.append(bullet)
 
     my_circle.move(command)
-
+    for bullet in bullets:
+        bullet.collision_check(enemys_list)
+        bullet.move()
+        
     #그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
     my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
     my_draw.ellipse(tuple(my_circle.position), outline = my_circle.outline, fill = (0, 0, 0))
-#좌표는 동그라미의 왼쪽 위, 오른쪽 아래 점 (x1, y1, x2, y2)
+    
+    for enemy in enemys_list:
+        if enemy.state != 'die':
+            my_draw.ellipse(tuple(enemy.position), outline = enemy.outline, fill = (255, 0, 0))
+
+    for bullet in bullets:
+        if bullet.state != 'hit':
+            my_draw.rectangle(tuple(bullet.position), outline = bullet.outline, fill = (0, 0, 255))
+
+    #좌표는 동그라미의 왼쪽 위, 오른쪽 아래 점 (x1, y1, x2, y2)
     joystick.disp.image(my_image)
+
+    #화면 갱신 속도
+    time.sleep(0.01)
---
+++ practice/test_anything.py
@@ -0,0 +1,2 @@
+disp.image(Image.open("led_pixels_mario.bmp")) # 이미지 띄우기 성공
+
